cca93037bd103d2c328e6a38f0f2a450d84e0f1a
bdash9 <5065324+bdash9@users.noreply.github.com>
2022-12-22 21:06:51 -0500

    Add files via upload

---
+++ Secrets.orig.py
@@ -0,0 +1,10 @@
+secrets = {
+    'ssid' : 'insert your network name here',
+    'password' : 'insert your network password here',
+      'aio_username' : "your-aio-username-here",
+    'aio_key' : 'your-aio-key-here',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'twitter_api_key' : 'insert your twitter api key here',
+    'twitter_secret_key' : 'insert your twitter secret key here',
+    'bearer_token' : 'insert your bearer token here'
+    }
