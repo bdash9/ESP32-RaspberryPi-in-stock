f0191f4f8beed41c3fed145d41e2d70e38c1c756
bdash9 <5065324+bdash9@users.noreply.github.com>
2022-12-22 21:09:14 -0500

    Update secrets.py

--- CircuitPython 7.x/secrets.py
+++ CircuitPython 7.x/secrets.py
@@ -1,10 +1,10 @@
 secrets = {
-    'ssid' : 'INDIGO',
-    'password' : 'redsun1234',
-      'aio_username' : "bdash9",
-    'aio_key' : 'aio_ADNF09xVCObkdsG2mXdSqgIIv6Mf',
+    'ssid' : 'insert your network name here',
+    'password' : 'insert your network password here',
+      'aio_username' : "your-aio-username-here",
+    'aio_key' : 'your-aio-key-here',
     'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
-    'twitter_api_key' : 'kAoK1evMqTTrQn2CrnyRRHqwG',
-    'twitter_secret_key' : 'R5Y4SM3qZByR6ZQVe3mCdFhPcb1x3YzbRSKUxkDP0hOPHyvMht',
-    'bearer_token' : 'AAAAAAAAAAAAAAAAAAAAAJWYkQEAAAAATby026%2Fj0KKjCVBDMIaaiVy5Bac%3Dxh7MdXUcp0cKtq9ELZqZI8Wem7HMcTIIXScwZXE5Dgbuvcvewe'
+    'twitter_api_key' : 'insert your twitter api key here',
+    'twitter_secret_key' : 'insert your twitter secret key here',
+    'bearer_token' : 'insert your bearer token here'
     }
